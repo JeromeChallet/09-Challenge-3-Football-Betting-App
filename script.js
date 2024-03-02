8ad032d4bcc2dc6b47964802274c00d7425a94ea
jrme421 <jeromepchallet@gmail.com>
2024-03-02 10:07:20 +0900

    final commit

--- script.js
+++ script.js
@@ -14,17 +14,34 @@ a
 */
 "use strict";
 const gameEvents = new Map([
-	[17, "⚽ GOAL"],
-	[36, "� Substitution"],
-	[47, "⚽ GOAL"],
-	[61, "� Substitution"],
-	[64, "� Yellow card"],
-	[69, "� Red card"],
-	[70, "� Substitution"],
-	[72, "� Substitution"],
-	[76, "⚽ GOAL"],
-	[80, "⚽ GOAL"],
-	[92, "� Yellow card"],
+  [17, "⚽ GOAL"],
+  [36, "� Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "� Substitution"],
+  [64, "� Yellow card"],
+  [69, "� Red card"],
+  [70, "� Substitution"],
+  [72, "� Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "� Yellow card"],
 ]);
 
-//1.Create an array called 'events' that contains the different game events that happened (no  duplicates)
+//1.
+const events = new Set(gameEvents.values());
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+
+// 3.
+const time = [...gameEvents.keys()].pop(); //retrieve the last element key of the map
+console.log(
+  `An event happened, on average, every ${time / gameEvents}  minutes`
+);
+
+// 4.
+for (const [min, events] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${events}`);
+}
